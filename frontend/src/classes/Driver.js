178ffdde09089985800b4355dbe23abd794cb42e
nathanliow <nathanliow7456@gmail.com>
2023-10-21 12:22:04 -0500

    Added driver

---
+++ frontend/src/classes/Driver.js
@@ -0,0 +1,10 @@
+class Driver {
+
+    constructor(name, carDescription, licensePlate, seatCapacity) {
+        this.name = name;
+        this.carDescription = carDescription;
+        this.licensePlate = licensePlate;
+        this.seatCapacity = seatCapacity;
+    }
+    
+}
