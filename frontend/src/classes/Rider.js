43e18bb2de06863908dd0b912bc7c99a14c8bf3b
txst54 <wang1024x768@gmail.com>
2023-10-21 12:20:06 -0500

    Update Rider.js

---
+++ frontend/src/classes/Rider.js
@@ -0,0 +1,8 @@
+class Rider {
+    Rider(name, email, phone, orgList) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.orgList = orgList;
+    }
+}
